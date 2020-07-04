4c3d5a6040e7c65c8069767ca0998c45cb55c1f2
Sumanth M <sumanthpdm@gmail.com>
2020-07-05 02:53:33 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 import flask
-import quiz_generator 
+import quiz_generator
 import os
 from os import path
 from flask import request
 from flask_cors import CORS
-import uuid 
+import uuid
 import re
 import json
 from datetime import datetime
@@ -15,39 +15,57 @@ CORS(app)
 app.config["DEBUG"] = True
 
 
-@app.route('/quiz', methods=['GET'])
-def quiz():
-    
-    text = request.args.get('text')
-    #text = "A tonsillectomy is the surgical removal of the palantine tonsils. And adenoidectomy is the surgical removal of the adenoids. Tube will be temporarily inserted through your mouth and into your throat to help you breathe during the operation. Tonsillectomy's and adenoidectomy's are rarely done under local anesthesia in adults and never in children"
-    quiz = quiz_generator.Quiz(transcribed_text=text)
-    response = quiz.generate_questions()
- 
-    return json.dumps(response)
-
-@app.route('/notes', methods=['GET'])
-def notes():
-    text = request.args.get('text')
-    print(text)
-    #text = "A tonsillectomy is the surgical removal of the palantine tonsils. And adenoidectomy is the surgical removal of the adenoids. Tube will be temporarily inserted through your mouth and into your throat to help you breathe during the operation. Tonsillectomy's and adenoidectomy's are rarely done under local anesthesia in adults and never in children"
+@app.route('/', methods=['POST'])
+def home():
+    req_data = request.get_json()
+    text = req_data['text']
 
     processor = text_processor.TextProcessor(text=text)
-    notes = processor.get_summary(ratio = 0.5)
+    notes = processor.get_summary(ratio=0.5)
+
+    quiz = quiz_generator.Quiz(transcribed_text=text)
+    quiz = quiz.generate_questions()
+
+    summary = processor.get_summary(word_count=100)
+    response = {
+        "summary": summary,
+        "notes": notes,
+        "quiz": quiz
+    }
+    return json.dumps(response)
+
+
+@app.route('/quiz', methods=['POST'])
+def quiz():
+
+    req_data = request.get_json()
+    text = req_data['text']
+
+    quiz = quiz_generator.Quiz(transcribed_text=text)
+    response = quiz.generate_questions()
+
+    return json.dumps(response)
+
+
+@app.route('/notes', methods=['POST'])
+def notes():
+    req_data = request.get_json()
+    text = req_data['text']
+
+    processor = text_processor.TextProcessor(text=text)
+    notes = processor.get_summary(ratio=0.5)
     return json.dumps(notes)
 
-@app.route('/summary', methods=['GET'])
+
+@app.route('/summary', methods=['POST'])
 def summary():
-    text = request.args.get('text')
-    #text = "A tonsillectomy is the surgical removal of the palantine tonsils. And adenoidectomy is the surgical removal of the adenoids. Tube will be temporarily inserted through your mouth and into your throat to help you breathe during the operation. Tonsillectomy's and adenoidectomy's are rarely done under local anesthesia in adults and never in children"
+    req_data = request.get_json()
+    text = req_data['text']
 
     processor = text_processor.TextProcessor(text=text)
     summary = processor.get_summary(word_count=100)
     return json.dumps(summary)
 
 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-    #print(summary())
-
